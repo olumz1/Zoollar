8e6b58ccc478bb7b4632ef91e60a0a776afab87e
olumz1 <olumz1@yahoo.com>
2023-01-08 21:22:16 +0000

    wip

---
+++ Zoollar.Properties.API/Data/IPropertyRepo.cs
@@ -0,0 +1,26 @@
+﻿using Zoollar.Properties.API.Models.Entities;
+
+namespace Zoollar.Properties.API.Data
+{
+    public interface IPropertyRepo
+    {
+        bool SaveChanges();
+
+        Task<IEnumerable<Property>> GetAllProperties();
+
+        Task<Property> GetPropertyById(Guid id);
+
+        Task CreateProperty(Property property);
+
+        Task DeleteProperty(Guid id);
+
+        Task UpdateProperty(Property property);
+
+        Task<IEnumerable<Property>> FilterPropertiesByLocation(string location);
+
+        Task<IEnumerable<Property>> FilterPropertiesByCity (string city);
+
+        Task<IEnumerable<Property>> GetPropertiesByAgentId(Guid AgentId);
+
+    }
+}
---
+++ Zoollar.Properties.API/Data/PropertyRepo.cs
@@ -0,0 +1,52 @@
+﻿using Zoollar.Properties.API.Models.Entities;
+
+namespace Zoollar.Properties.API.Data
+{
+    public class PropertyRepo : IPropertyRepo
+    {
+        public Task CreateProperty(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProperty(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> GetAllProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Property> GetPropertyById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProperty(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> FilterPropertiesByLocation(string location) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> FilterPropertiesByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> FilterPropertiesByAgentId(Guid AgentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
