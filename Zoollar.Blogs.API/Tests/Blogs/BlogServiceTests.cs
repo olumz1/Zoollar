667923fca93ae75785bdab6b8300dec16ea18778
olumz1 <olumz1@yahoo.com>
2023-01-19 14:06:27 +0000

    wip

--- Zoollar.Blogs.API/Tests/Blogs/BlogServiceTests.cs
+++ Zoollar.Blogs.API/Tests/Blogs/BlogServiceTests.cs
@@ -1,15 +1,12 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using Zoollar.Blogs.API.Common;
+using Zoollar.Blogs.API.Data;
+using Zoollar.Blogs.API.Dtos;
 using Zoollar.Blogs.API.Models.Entities;
 using Zoollar.Blogs.API.Services;
-using FluentAssertions;
-using Zoollar.Blogs.API.Dtos;
-using Zoollar.Blogs.API.Data;
-using System.Reflection.PortableExecutable;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Zoollar.Blogs.API.Tests.Blogs
 {
@@ -152,45 +149,6 @@ namespace Zoollar.Blogs.API.Tests.Blogs
             blog.Should().BeNull();
 
             _dbContext.Database.EnsureDeleted();
-        }
-
-        public static Blog[] Blogs()
-        {
-            return new Blog[] { 
-                new Blog
-                {
-                    Id = new Guid("{17C73ADB-D5B6-46BD-9EAF-6D54FB14E13F}"),
-                    Header = "This is the first header 1",
-                    Author = "TestUser1",
-                    Description = "This is the blog description 1",
-                    Image = "image.jpg",
-                    IsVisible = true,
-                    Profession = "Blog Profession1",
-                    PublishedDate = new DateTime(2023, 10, 3)
-                },
-                new Blog
-                {
-                    Id = new Guid("{DB66DEA2-22C0-4415-940E-22F65033145F}"),
-                    Header = "This is the first header",
-                    Author = "TestUser",
-                    Description = "This is the blog description",
-                    Image = "image.jpg",
-                    IsVisible = true,
-                    Profession = "Blog Profession",
-                    PublishedDate = new DateTime(2023, 8, 13)
-                },
-                new Blog
-                {
-                   Id = new Guid("{9F113F49-BB21-4934-9090-75D3875D026B}"),
-                   Header = "This is the first header",
-                   Author = "TestUser",
-                   Description = "This is the blog description",
-                   Image = "image.jpg",
-                   IsVisible = true,
-                   Profession = "Blog Profession",
-                   PublishedDate = new DateTime(2022, 10, 11)
-                } 
-            };
         }
     }
 }
