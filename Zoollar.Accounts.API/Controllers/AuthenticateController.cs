ecb2ab97508878b7e54b2b5affbb12ce7b0cf648
olumz1 <olumz1@yahoo.com>
2024-02-06 13:55:11 +0000

    wip

--- Zoollar.Accounts.API/Controllers/AuthenticateController.cs
+++ Zoollar.Accounts.API/Controllers/AuthenticateController.cs
@@ -1,28 +1,28 @@
-﻿using JwtAuthenticationManager;
-using JwtAuthenticationManager.Models;
-using Microsoft.AspNetCore.Mvc;
-using Zoollar.Accounts.API.Services.UserAccount;
+﻿//using JwtAuthenticationManager;
+//using JwtAuthenticationManager.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Zoollar.Accounts.API.Services.UserAccount;
 
-namespace Zoollar.Accounts.API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthenticateController : ControllerBase
-    {
-        private readonly JwtTokenHandler _jwtTokenHandler;
+//namespace Zoollar.Accounts.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AuthenticateController : ControllerBase
+//    {
+//        private readonly JwtTokenHandler _jwtTokenHandler;
 
-        public AuthenticateController(JwtTokenHandler jwtTokenHandler)
-        {
-            _jwtTokenHandler = jwtTokenHandler;
-        }
+//        public AuthenticateController(JwtTokenHandler jwtTokenHandler)
+//        {
+//            _jwtTokenHandler = jwtTokenHandler;
+//        }
 
 
-        [HttpPost]
-        public ActionResult<AuthenticationResponse?> Login([FromBody] AuthenticationRequest authenticationRequest)
-        {
-            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(authenticationRequest);
-            if (authenticationResponse == null) return Unauthorized();
-            return Ok(authenticationResponse);
-        }
-    }
-}
+//        [HttpPost]
+//        public ActionResult<AuthenticationResponse?> Login([FromBody] AuthenticationRequest authenticationRequest)
+//        {
+//            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(authenticationRequest);
+//            if (authenticationResponse == null) return Unauthorized();
+//            return Ok(authenticationResponse);
+//        }
+//    }
+//}
